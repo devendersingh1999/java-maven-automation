89edfd6662de7083f6806153134fb782dac76c3f
devendersingh1999 <devendersingh29121999@gmail.com>
2025-11-15 12:08:57 +0000

    added new line

--- src/main/java/com/Hi.java
+++ src/main/java/com/Hi.java
@@ -3,6 +3,6 @@ package com;
 public class Hi {
     public static void main(String[] args) {
 
-        System.out.println("hi evey one i am sagar");
+        System.out.println("hi evey one i am sagar this is new line");
     }
 }
