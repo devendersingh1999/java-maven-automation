e862ff8a9ad0d4c2938204616ab2407bc20ae972
devendersingh1999 <devendersingh29121999@gmail.com>
2025-11-15 13:00:30 +0000

    jenkinsfile update

--- src/main/java/com/example/Hi.java
+++ src/main/java/com/example/Hi.java
@@ -3,5 +3,6 @@ package com.example;
 public class Hi {
     public static void main(String[] args) {
         System.out.println("this is greeting message from sagar");
+    System.out.println("hey there");
     }
 }
