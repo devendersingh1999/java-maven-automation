5da902c2e3c3355491cd674d99dfc1e086458a4e
devendersingh1999 <devendersingh29121999@gmail.com>
2025-11-15 12:27:40 +0000

    greeting

--- src/main/java/com/Hi.java
+++ src/main/java/com/Hi.java
@@ -3,6 +3,7 @@ package com;
 public class Hi {
     public static void main(String[] args) {
 
-        System.out.println("hi evey one i am sagar this is new line");
+        System.out.println("hi evey one i am sagar, this is new line");
+        System.out.println("this is greating message");
     }
 }
